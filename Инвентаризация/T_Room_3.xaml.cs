e1386535a60db948e53e95fde0b6262a8b368d92
gr691_gaa <toni.goiman.01@mail.ru>
2021-04-08 17:14:19 +0700

    Создал редактирование 1 комнаты

---
+++ Инвентаризация/T_Room_3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Инвентаризация
+{
+    /// <summary>
+    /// Логика взаимодействия для T_Room_3.xaml
+    /// </summary>
+    public partial class T_Room_3 : Window
+    {
+        public T_Room_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
