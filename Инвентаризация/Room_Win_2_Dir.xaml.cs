9184f00f22ad090565f20b9913875a96e474b1aa
gr691_tkp <kirill_170717@list.ru>
2021-04-13 14:17:02 +0700

    Создал окна для директора

---
+++ Инвентаризация/Room_Win_2_Dir.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Инвентаризация
+{
+    /// <summary>
+    /// Логика взаимодействия для Room_Win_2_Dir.xaml
+    /// </summary>
+    public partial class Room_Win_2_Dir : Window
+    {
+        public Room_Win_2_Dir()
+        {
+            InitializeComponent();
+        }
+    }
+}
